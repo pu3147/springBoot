8e7595edb40abd5151ad16361973b16f7d27532e
freeLess <gao9929@163.com>
2021-03-31 22:55:11 +0800

    spring-boot-hello

--- src/main/java/cc/fbksy/edu/springBoot/home/HomeController.java
+++ src/main/java/cc/fbksy/edu/springBoot/home/HomeController.java
@@ -1,14 +1,20 @@
 package cc.fbksy.edu.springBoot.home;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private StringRedisTemplate template;
 
     @GetMapping(value = { "/", "/index"})
-    public String login() {
+    public String welcomeIndex() {
+        template.opsForValue().increment("totalSize");
+        System.out.println(template.opsForValue().get("totalSize"));
         return "index";
     }
 }
